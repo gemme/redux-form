5328928f94ecf310e3a09432d3372000297b1063
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2018-10-04 21:10:27 -0500

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import RegisterForm from './components/RegisterForm';
+
+class RegisterFormContainer extends Component{
+
+  submit = values => {
+    alert(JSON.stringify(values, null, 4));
+  }
+
+  render() {
+    return <RegisterForm onSubmit={this.submit}/>
+  }
+}
+
+export default RegisterFormContainer;
