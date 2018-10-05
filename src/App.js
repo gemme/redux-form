6710832ba2efc67d1a871166243c30e135ad582d
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2018-10-04 21:40:17 -0500

    set initial values

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ class RegisterFormContainer extends Component{
     alert(JSON.stringify(values, null, 4));
   }
 
+  getInitialValues () {
+    return {
+      preference: 'spaces',
+      newsletter: true
+    }
+  }
+
+
   render() {
-    return <RegisterForm onSubmit={this.submit}/>
+    return <RegisterForm
+      onSubmit={this.submit}
+      initialValues={this.getInitialValues()}/>
   }
 }
 
